2d9bb9c7c00a167f3ab67fbe4410f07d85254128
Nicolás Rosselló <69722680+rossellonico@users.noreply.github.com>
2023-09-07 11:12:22 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,8 +1,15 @@
-﻿internal class Program
+﻿
+using CursoGit;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        //Invocar 2 ejemplos para cada operación de la clas Calculator.
+        Calculator  c = new Calculator();
+        double add1= c.Add(5.1 , 5.4);
+        Console.WriteLine("Add1= " + add1);
+        double add2= c.Add(5.5 , 5.9);
+        Console.WriteLine("Add1= " + add2);
     }
 
 
