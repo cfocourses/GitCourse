f4819487b2c6fd673ea2297762acae102b709d01
mgiorgi32 <marcosgiorgi2004@gmail.com>
2023-09-07 11:16:36 -0300

    correcion calculos

--- Program.cs
+++ Program.cs
@@ -5,13 +5,14 @@ internal class Program
     private static void Main(string[] args)
     {
         Calculator c = new Calculator();
-        c.Abs(2,7);
-        c.Abs(8, 9);
-        c.Abs(3, 2);
-        c.Abs(0, 1);
-        c.AbsMod(1, 5, 7);
-        c.AbsMod(7,9,3);
-        c.AbsMod(2, 1, 0);
+        double abs1 = c.Abs(2,7);
+        double abs2 = c.Abs(8, 9);
+        Console.WriteLine("Abs1="+ abs1);
+        Console.WriteLine("Abs2=" + abs2);
+        double absMod1= c.AbsMod(7,9,3);
+        double absMod2 = c.AbsMod(2, 1, 0);
+        Console.WriteLine("absMod1 = "+ absMod1);
+        Console.WriteLine("absMod2 = " + absMod2);
     }
 
 
