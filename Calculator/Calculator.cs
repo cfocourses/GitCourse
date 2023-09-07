ad7e68ead49741a2a7f4938c1282b7550d05a5d1
zhither <edurodriguez@cfotechlatam.com>
2023-09-07 11:09:38 -0300

    suma

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,52 @@
+﻿namespace CursoGit;
+
+public class Calculator : ICalculator
+{
+    public double Abs(double x, int module)
+    {
+        throw new NotImplementedException();
+    }
+
+    public double Add(double a, double y)
+    {
+        return a * y;
+        throw new NotImplementedException();
+    }
+
+    public double Div(double x, double y)
+    {
+        throw new NotImplementedException();
+    }
+
+    public double Mult(double x, double y)
+    {
+        return x * y
+        throw new NotImplementedException();
+    }
+
+     public double AddMod(double a, double y, int mod)
+     {
+          throw new NotImplementedException(); 
+     }
+
+     public double MultMod(double x, double y, int mod)
+     {
+         throw new NotImplementedException(); 
+     }
+
+     public double DivMod(double x, double y, int mod)
+     {
+         throw new NotImplementedException(); 
+     }
+
+     public double AbsMod(double x, int module, int mod)
+     {
+         throw new NotImplementedException(); 
+     }
+
+    public double Pow(double x, int n)
+    {
+        throw new NotImplementedException();
+    }
+}
+
