bc823d4786210970af213a719a91fdce86f3e217
mgiorgi32 <marcosgiorgi2004@gmail.com>
2023-09-07 11:10:21 -0300

    numeros para hacer calculos

--- Program.cs
+++ Program.cs
@@ -1,8 +1,17 @@
-﻿internal class Program
+﻿using CursoGit;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        //Invocar 2 ejemplos para cada operación de la clas Calculator.
+        Calculator c = new Calculator();
+        c.Abs(2,7);
+        c.Abs(8, 9);
+        c.Abs(3, 2);
+        c.Abs(0, 1);
+        c.AbsMod(1, 5, 7);
+        c.AbsMod(7,9,3);
+        c.AbsMod(2, 1, 0);
     }
 
 
