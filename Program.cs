cbc8a0e876249a4f477365d74806034841ef1f33
Nicolás Rosselló <69722680+rossellonico@users.noreply.github.com>
2023-09-07 11:57:54 -0300

    merge

--- Program.cs
+++ Program.cs
@@ -8,7 +8,6 @@ internal class Program
         //Invocar 2 ejemplos para cada operación de la clas Calculator.
         Calculator  c = new Calculator();
         c.Mult(2,5);
-        Calculator  c = new Calculator();
         double add1= c.Add(5.1 , 5.4);
         Console.WriteLine("Add1= " + add1);
         double add2= c.Add(5.5 , 5.9);
