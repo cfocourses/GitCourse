949b0664b1bad738265399122d3397235ef79bab
LukeDev89 <78065193+LukeDev89@users.noreply.github.com>
2023-09-07 11:49:36 -0300

    shapes

--- Program.cs
+++ Program.cs
@@ -1,12 +1,19 @@
 ﻿using CursoGit;
+using CursoGit.Shapes;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
         var calculator = new Calculator();
+        var circle = new Circle();
+        var triangle = new Triangule();
 
         Console.WriteLine($"3 + 5: {calculator.Add(3, 5)}");
         Console.WriteLine($"6^2: {calculator.Pow(6, 2)}");
+
+        Console.WriteLine();
+        Console.WriteLine($"Area de circulo r5: {circle.Area(5,5)}");
+        Console.WriteLine($"Perimetro de triangulo b3 h4: {triangle.Perimeter(3,4)}");
     }
 }
